81fd964a54e6d13e32fd4a070caeb5c9a847c9cf
breitburg <ilya.breitburg@gmail.com>
2022-01-05 15:58:36 +0300

    Final touches

--- NeonSign.h
+++ NeonSign.h
@@ -1,19 +1,22 @@
+#include "HomeSpan.h" 
 #include "extras/PwmPin.h"
 
 struct NeonSign : Service::LightBulb {
   LedPin *ledPin;
   SpanCharacteristic *power;
   SpanCharacteristic *level;
-  
+
   NeonSign(int pin) : Service::LightBulb() {
     power = new Characteristic::On();
-    level = new Characteristic::Brightness(100);
-    level->setRange(5, 100, 1);
-    this->ledPin=new LedPin(pin);
+    level = new Characteristic::Brightness(80);
+    level->setRange(5, 80, 1);
+    
+    this->ledPin = new LedPin(pin);
+    analogWrite(pin, 80);
   }
 
   boolean update() {
-    ledPin->set(power->getNewVal()*level->getNewVal());
+    ledPin->set(power->getNewVal() * level->getNewVal());
     return(true);
   }
 };
