30db5b98c6e689924c3a51326e3ba23d1327e177
breitburg <ilya.breitburg@gmail.com>
2022-01-05 15:59:55 +0300

    Added Wi-Fi credentials

--- SonicOS.ino
+++ SonicOS.ino
@@ -3,25 +3,23 @@
 
 void setup() {
   Serial.begin(115200);
-  homeSpan.setApSSID("Sonic Neon Sign");
-  homeSpan.setApPassword("supersonic");
-  homeSpan.setApTimeout(3600);
-  homeSpan.enableAutoStartAP();
   
-  homeSpan.begin(Category::Lighting, "Neon Sign", "SonicNeonSign", "Prototype");
+  homeSpan.setPairingCode("42156873");
+  homeSpan.setWifiCredentials("4GUFI_5047", "1234567890");
+  homeSpan.begin(Category::Lighting, "Neon Sign", "SonicNeonSign", "P1ECE");
   
   new SpanAccessory();
     new Service::AccessoryInformation();    
-      new Characteristic::Name("Neon Sign");    
-      new Characteristic::Manufacturer("Breitburg Ilya"); 
-      new Characteristic::SerialNumber("Prototype");  
-      new Characteristic::Model("Sonic Neon Sign");   
-      new Characteristic::FirmwareRevision("1.0");  
-      new Characteristic::Identify();               
+      new Characteristic::Name("Neon Sign");
+      new Characteristic::Manufacturer("Breitburg Ilya");
+      new Characteristic::SerialNumber("P1ECE");
+      new Characteristic::Model("Sonic Neon Sign");
+      new Characteristic::FirmwareRevision("1.1.0");
+      new Characteristic::Identify();
       
     new Service::HAPProtocolInformation();
       new Characteristic::Version("1.1.0");
-  
+    
     new NeonSign(2);
 }
 
